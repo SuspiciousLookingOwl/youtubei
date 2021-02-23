54c239fc0fc4d13a661158e0068f7671d4c810ab
VincentJonathan <vinjongm@gmail.com>
2021-02-24 04:13:56 +0700

    add comment test

---
+++ tests/Comment.spec.ts
@@ -0,0 +1,13 @@
+import { Comment, Video } from "../src";
+import "jest-extended";
+
+describe("Comment", () => {
+	it("url should match", () => {
+		const comment = new Comment({
+			id: "commentId",
+			video: new Video({ id: "videoId" }),
+		});
+
+		expect(comment.url).toBe("https://www.youtube.com?watch=videoId&lc=commentId");
+	});
+});
