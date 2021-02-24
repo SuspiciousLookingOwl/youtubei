9fbe74d4912fe123e7072e24e16ed4f7c870487f
VincentJonathan <vinjongm@gmail.com>
2021-02-24 11:45:01 +0700

    add mixins

--- src/common/index.ts
+++ src/common/index.ts
@@ -2,3 +2,4 @@ export { default as http } from "./http";
 export * from "./helper";
 export * from "./types";
 export * from "./decorators";
+export { default as applyMixins } from "./mixins";
---
+++ src/common/mixins.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
+export default (derivedCtor: any, baseCtors: any[]) => {
+	baseCtors.forEach((baseCtor) => {
+		Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
+			if (name !== "constructor") {
+				derivedCtor.prototype[name] = baseCtor.prototype[name];
+			}
+		});
+	});
+};
