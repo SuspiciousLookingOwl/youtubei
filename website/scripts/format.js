b256d326f38985973fdb61e83005ca7192535b90
VincentJonathan <vinjongm@gmail.com>
2021-02-26 03:08:14 +0700

    dont remove hierarchy section

--- website/scripts/format.js
+++ website/scripts/format.js
@@ -6,13 +6,10 @@
 
 const fs = require("fs");
 
-const excludedHierarchy = ["searchresult.md", "thumbnails.md"];
-
 const files = fs.readdirSync("./docs/api/classes");
 
 for (const fileName of files) {
 	fs.readFile(`./docs/api/classes/${fileName}`, "utf-8", (_err, file) => {
-		if (!excludedHierarchy.includes(fileName)) file = file.replace(/## Hierarchy.*?##/s, "##");
 		file = file.replace(/## Implements.*?##/s, "##");
 		// eslint-disable-next-line @typescript-eslint/no-empty-function
 		fs.writeFile(`./docs/api/classes/${fileName}`, file, () => {});
