430cedbfad3d65a8838f34873e08cbebf3bdb377
VincentJonathan <vinjongm@gmail.com>
2021-03-01 01:04:03 +0700

    add no-non-null-assertion

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
 	rules: {
 		"linebreak-style": ["error", "unix"],
 		"no-mixed-spaces-and-tabs": "off",
+		"no-non-null-assertion": "off",
 		indent: ["off", "tab"],
 		quotes: ["error", "double"],
 		semi: ["error", "always"],
