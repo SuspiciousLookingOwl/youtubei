ca0cc613a1ce2b2093570adb58049c044a4c0d40
カグチ <59391215+KagChi@users.noreply.github.com>
2021-09-17 10:48:24 +0700

    chore: remove unused attributes

--- src/classes/MixPlaylist.ts
+++ src/classes/MixPlaylist.ts
@@ -20,15 +20,7 @@ export default class MixPlaylist extends Base implements PlaylistAttributes {
     /** How many videos in this playlist */
     videoCount!: number;
     /** How many viewers does this playlist have */
-    viewCount!: number | null;
-    /** Last time this playlist is updated */
-    lastUpdatedAt!: string | null;
-    /** The channel that made this playlist */
-    channel?: ChannelCompact;
-    /** Videos in the playlist */
     videos: VideoCompact[] = [];
-    /** Current continuation token to load next videos  */
-    continuation!: string | undefined;
 
     /** @hidden */
     constructor(playlist: Partial<MixPlaylist> = {}) {
@@ -46,11 +38,7 @@ export default class MixPlaylist extends Base implements PlaylistAttributes {
         const playlist = twoColumnWatchNextResults.playlist.playlist
         this.title = playlist.titleText.simpleText;
         this.id = playlist.playlistId;
-
         this.videoCount = playlist.contents.length;
-        this.viewCount = null;
-        this.lastUpdatedAt = null;
-
         this.videos = MixPlaylist.parseVideos(playlist.contents, this);
         return this;
     }
