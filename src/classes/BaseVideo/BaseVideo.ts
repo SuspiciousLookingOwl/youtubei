ca426779e0de02e3b64d83e0e69e33091272e527
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-07-10 09:39:13 +0700

    refactor base video parser

---
+++ src/classes/BaseVideo/BaseVideo.ts
@@ -0,0 +1,96 @@
+import {
+	Base,
+	BaseAttributes,
+	ChannelCompact,
+	PlaylistCompact,
+	Thumbnails,
+	VideoCompact,
+} from "..";
+import { getContinuationFromItems, YoutubeRawData } from "../../common";
+import { I_END_POINT } from "../../constants";
+import { BaseVideoParser } from "./BaseVideoParser";
+
+/** @hidden */
+export interface BaseVideoAttributes extends BaseAttributes {
+	title: string;
+	thumbnails: Thumbnails;
+	description: string;
+	channel: ChannelCompact;
+	uploadDate: string;
+	viewCount: number | null;
+	likeCount: number | null;
+	isLiveContent: boolean;
+	tags: string[];
+	related: (VideoCompact | PlaylistCompact)[];
+	relatedContinuation?: string;
+}
+
+/** Represents a Video  */
+export default class BaseVideo extends Base implements BaseVideoAttributes {
+	/** The title of this video */
+	title!: string;
+	/** Thumbnails of the video with different sizes */
+	thumbnails!: Thumbnails;
+	/** The description of this video */
+	description!: string;
+	/** The channel that uploaded this video */
+	channel!: ChannelCompact;
+	/** The date this video is uploaded at */
+	uploadDate!: string;
+	/** How many view does this video have, null if the view count is hidden */
+	viewCount!: number | null;
+	/** How many like does this video have, null if the like count hidden */
+	likeCount!: number | null;
+	/** Whether this video is a live content or not */
+	isLiveContent!: boolean;
+	/** The tags of this video */
+	tags!: string[];
+	/** Videos / playlists related to this video  */
+	related: (VideoCompact | PlaylistCompact)[] = [];
+	/** Current continuation token to load next related content  */
+	relatedContinuation?: string;
+
+	/** @hidden */
+	constructor(video: Partial<BaseVideoAttributes> = {}) {
+		super();
+		Object.assign(this, video);
+	}
+
+	/**
+	 * Load this instance with raw data from Youtube
+	 *
+	 * @hidden
+	 */
+	load(data: YoutubeRawData): BaseVideo {
+		BaseVideoParser.loadBaseVideo(this, data);
+		return this;
+	}
+
+	/**
+	 * Video / playlist to play next after this video, alias to
+	 * ```js
+	 * video.related[0]
+	 * ```
+	 */
+	get upNext(): VideoCompact | PlaylistCompact {
+		return this.related[0];
+	}
+
+	/** Load next related videos / playlists */
+	async nextRelated(count = 1): Promise<(VideoCompact | PlaylistCompact)[]> {
+		const newRelated: (VideoCompact | PlaylistCompact)[] = [];
+		for (let i = 0; i < count || count == 0; i++) {
+			if (this.relatedContinuation === undefined) break;
+
+			const response = await this.client.http.post(`${I_END_POINT}/next`, {
+				data: { continuation: this.relatedContinuation },
+			});
+
+			newRelated.push(...BaseVideoParser.parseRelated(response.data, this.client));
+			this.relatedContinuation = getContinuationFromItems(response.data);
+		}
+
+		this.related.push(...newRelated);
+		return newRelated;
+	}
+}
---
+++ src/classes/BaseVideo/BaseVideoParser.ts
@@ -0,0 +1,108 @@
+import { getContinuationFromItems, stripToInt, YoutubeRawData } from "../../common";
+import ChannelCompact from "../ChannelCompact";
+import Client from "../Client";
+import PlaylistCompact from "../PlaylistCompact";
+import Thumbnails from "../Thumbnails";
+import VideoCompact from "../VideoCompact";
+import BaseVideo from "./BaseVideo";
+
+export class BaseVideoParser {
+	static loadBaseVideo(target: BaseVideo, data: YoutubeRawData): BaseVideo {
+		const videoInfo = BaseVideoParser.parseRawData(data);
+
+		// Basic information
+		target.id = videoInfo.videoDetails.videoId;
+		target.title = videoInfo.videoDetails.title;
+		target.uploadDate = videoInfo.dateText.simpleText;
+		target.viewCount = +videoInfo.videoDetails.viewCount || null;
+		target.isLiveContent = videoInfo.videoDetails.isLiveContent;
+		target.thumbnails = new Thumbnails().load(videoInfo.videoDetails.thumbnail.thumbnails);
+
+		// Channel
+		const { title, thumbnail, subscriberCountText } = videoInfo.owner.videoOwnerRenderer;
+
+		target.channel = new ChannelCompact({
+			client: target.client,
+			id: title.runs[0].navigationEndpoint.browseEndpoint.browseId,
+			name: title.runs[0].text,
+			subscriberCount: subscriberCountText?.simpleText,
+			thumbnails: new Thumbnails().load(thumbnail.thumbnails),
+		});
+
+		// Like Count and Dislike Count
+		const topLevelButtons = videoInfo.videoActions.menuRenderer.topLevelButtons;
+		target.likeCount = stripToInt(BaseVideoParser.parseButtonRenderer(topLevelButtons[0]));
+
+		// Tags and description
+		target.tags =
+			videoInfo.superTitleLink?.runs
+				?.map((r: YoutubeRawData) => r.text.trim())
+				.filter((t: string) => t) || [];
+		target.description =
+			videoInfo.description?.runs.map((d: Record<string, string>) => d.text).join("") || "";
+
+		// Up Next and related videos
+		target.related = [];
+		const secondaryContents =
+			data[3].response.contents.twoColumnWatchNextResults.secondaryResults.secondaryResults
+				.results;
+
+		if (secondaryContents) {
+			target.related.push(...BaseVideoParser.parseRelated(secondaryContents, target.client));
+			// Related continuation
+			target.relatedContinuation = getContinuationFromItems(secondaryContents);
+		} else {
+			target.related = [];
+		}
+
+		return target;
+	}
+
+	static parseRelated(data: YoutubeRawData, client: Client): (VideoCompact | PlaylistCompact)[] {
+		const secondaryContents: YoutubeRawData[] =
+			data.onResponseReceivedEndpoints[0].appendContinuationItemsAction.continuationItems;
+
+		return secondaryContents
+			.map((c: YoutubeRawData) => BaseVideoParser.parseCompactRenderer(c, client))
+			.filter((c): c is VideoCompact | PlaylistCompact => c !== undefined);
+	}
+
+	static parseContinuation(data: YoutubeRawData): string | undefined {
+		const secondaryContents =
+			data.onResponseReceivedEndpoints[0].appendContinuationItemsAction.continuationItems;
+
+		return getContinuationFromItems(secondaryContents);
+	}
+
+	private static parseRawData(data: YoutubeRawData): YoutubeRawData {
+		const contents =
+			data[3].response.contents.twoColumnWatchNextResults.results.results.contents;
+
+		const primaryInfo = contents.find((c: YoutubeRawData) => "videoPrimaryInfoRenderer" in c)
+			.videoPrimaryInfoRenderer;
+		const secondaryInfo = contents.find(
+			(c: YoutubeRawData) => "videoSecondaryInfoRenderer" in c
+		).videoSecondaryInfoRenderer;
+		const videoDetails = data[2].playerResponse.videoDetails;
+		return { ...secondaryInfo, ...primaryInfo, videoDetails };
+	}
+
+	private static parseCompactRenderer(
+		data: YoutubeRawData,
+		client: Client
+	): VideoCompact | PlaylistCompact | undefined {
+		if ("compactVideoRenderer" in data) {
+			return new VideoCompact({ client }).load(data.compactVideoRenderer);
+		} else if ("compactRadioRenderer" in data) {
+			return new PlaylistCompact({ client }).load(data.compactRadioRenderer);
+		}
+	}
+
+	private static parseButtonRenderer(data: YoutubeRawData): string {
+		const buttonRenderer = data.toggleButtonRenderer || data.buttonRenderer;
+		const accessibilityData = (
+			buttonRenderer.defaultText?.accessibility || buttonRenderer.accessibilityData
+		).accessibilityData;
+		return accessibilityData.label;
+	}
+}
