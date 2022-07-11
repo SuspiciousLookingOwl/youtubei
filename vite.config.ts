bab142ab7e57e52b247b56a1fb14f6b9d2075933
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-07-11 19:58:11 +0700

    build using tsc

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,8 @@
 /// <reference types="vitest" />
-import path from "path";
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-
-const external = ["node-fetch", "url", "events"];
 
 export default defineConfig({
-	plugins: [
-		dts({
-			entryRoot: "./src",
-		}),
-	],
 	test: {
 		globals: true,
-	},
-	esbuild: {
-		minify: false,
-	},
-	build: {
-		outDir: "dist",
-		emptyOutDir: true,
-		minify: false,
-		lib: {
-			entry: path.resolve(__dirname, "src/index.ts"),
-			name: "youtubei",
-			fileName: (format) => `index.${format}.js`,
-		},
-		rollupOptions: {
-			external,
-			output: {
-				globals: Object.fromEntries(external.map((e) => [e, e])),
-				inlineDynamicImports: true,
-			},
-		},
 	},
 });
