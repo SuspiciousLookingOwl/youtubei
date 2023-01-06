879ba1fc9d846e0549360baf9be71dbed3abdc77
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2023-01-06 17:35:12 +0700

    fix exclude path

--- docs/plugins.js
+++ docs/plugins.js
@@ -5,7 +5,7 @@ module.exports = [
 			entryPoints: ["../src/index.ts"],
 			mode: "file",
 			tsconfig: "../tsconfig.json",
-			exclude: ["**/common/**/*", "**/constants.ts", "**/*Parser.ts"],
+			exclude: ["**/common/utils/**/*", "**/constants.ts", "**/*Parser.ts"],
 			excludePrivate: true,
 			excludeProtected: true,
 			ignoreCompilerErrors: true,
