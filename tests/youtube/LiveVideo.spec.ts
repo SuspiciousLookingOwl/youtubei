b23982b4f3a63d10eff7d44b765aadf2151f699c
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2023-03-04 23:12:53 +0700

    use valid live video

--- tests/youtube/LiveVideo.spec.ts
+++ tests/youtube/LiveVideo.spec.ts
@@ -8,7 +8,7 @@ describe("Video", () => {
 	let video: LiveVideo;
 
 	beforeAll(async () => {
-		video = (await youtube.getVideo("JQnxefImhu8")) as LiveVideo;
+		video = (await youtube.getVideo("IOJO0zj55jo")) as LiveVideo;
 	});
 
 	it("match live getVideo result", () => {
