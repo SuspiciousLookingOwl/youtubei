d7abd52978ffc0d739ff5643991552e0d9cc3269
SuspiciousLookingOwl <vinjongm@gmail.com>
2024-10-26 15:37:09 +0700

    use live reply data

--- tests/youtube/Reply.spec.ts
+++ tests/youtube/Reply.spec.ts
@@ -1,19 +1,18 @@
 import "jest-extended";
 
-import { Client, Reply } from "../../src";
-import commentReplyRenderer from "./data/commentReplyRenderer.json";
+import { Client, Video } from "../../src";
 
-describe("Chat", () => {
-	it("match chat load", () => {
-		const reply = new Reply({ client: new Client() }).load(commentReplyRenderer);
+describe("Reply", () => {
+	let client: Client;
+	let video: Video;
+	beforeAll(async () => {
+		client = new Client();
+		video = (await client.getVideo("PziYflu8cB8")) as Video;
+		await video.comments.next();
+	});
 
-		expect(reply.id).toBe("UgwxLeoyuMjwpVQZjP14AaABAg.9KBiYFA7H5w9KGoxAGz1EC");
-		expect(reply.content).toBe(" @Adam Henriksson  Makes sense.");
-		expect(reply.publishDate).toBe("1 day ago");
-		expect(reply.likeCount).toBe(0);
-		expect(reply.isAuthorChannelOwner).toBeFalse();
-		expect(reply.author.id).toBe("UC6JbD_MR9t0-riw1j4ZuE9Q");
-		expect(reply.author.name).toBe("LongJourneys");
-		expect(reply.author.thumbnails?.length).toBe(3);
+	it("match reply load", async () => {
+		const replies = await video.comments.items[0].replies.next();
+		expect(replies.length).toBeGreaterThanOrEqual(1);
 	});
 });
